b251d80d554230a17998651d4b89b8bb30f764aa
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-19 13:50:25 -0300

    chore: add fastify

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-interface IUser {
-  name: string;
-  age: number;
-}
+import fastify from "fastify";
 
-console.log("Hello World!!")
+const app = fastify();
+
+app.get("/", () => {
+  return "Hello world!"
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running");
+});
