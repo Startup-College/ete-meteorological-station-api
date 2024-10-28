90acd0149d235de70ea20f09c32a3ac0d1507ecc
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-28 11:28:41 -0300

    chore: add tsup and config build in a package.json

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,6 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(exampleRoute);
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running');
 });
