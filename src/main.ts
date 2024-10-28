687675b6221b0da7278cbc68a62928d96e368460
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-28 10:50:00 -0300

    chore: add type provider zod

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import fastify from "fastify";
+import fastify from 'fastify';
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { exampleRoute } from './routes/example-route';
 
 const app = fastify();
 
-app.get("/", () => {
-  return "Hello world!"
-})
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(exampleRoute);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server running");
+  console.log('HTTP server running');
 });
---
+++ src/routes/example-route.ts
@@ -0,0 +1,35 @@
+import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+
+export function exampleRoute(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/example-create',
+    {
+      schema: {
+        body: z.object({
+          title: z.string().min(4),
+          details: z.string().nullable(),
+          quantity: z.number().int().positive().nullable()
+        }),
+        response: {
+          201: z.object({
+            id: z.string().uuid()
+          })
+        }
+      }
+    },
+    async (request, reply) => {
+      const { title, details, quantity } = request.body;
+
+      console.log(`
+      title: ${title} 
+      details: ${details}
+      quantity: ${quantity}`);
+
+      const id = crypto.randomUUID().toString();
+
+      return reply.status(201).send({ id });
+    }
+  );
+}
