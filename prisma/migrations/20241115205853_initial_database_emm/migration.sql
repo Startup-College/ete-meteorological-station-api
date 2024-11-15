a34774678ad21acfa49303c21da3e0938ac35cc1
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-11-15 19:22:22 -0300

    feat: implemented model user prisma

--- prisma/migrations/20241115205853_initial_database_emm/migration.sql
+++ prisma/migrations/20241115205853_initial_database_emm/migration.sql
@@ -2,17 +2,18 @@
 CREATE TABLE "stations" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "latitude" TEXT NOT NULL,
-    "longitude" TEXT NOT NULL,
+    "latitude" TEXT,
+    "longitude" TEXT,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT,
 
     CONSTRAINT "stations_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "readings" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
     "station_id" TEXT NOT NULL,
     "date_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "temperature" DOUBLE PRECISION NOT NULL,
@@ -21,6 +22,20 @@ CREATE TABLE "readings" (
 
     CONSTRAINT "readings_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "stations" ADD CONSTRAINT "stations_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "readings" ADD CONSTRAINT "readings_station_id_fkey" FOREIGN KEY ("station_id") REFERENCES "stations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
