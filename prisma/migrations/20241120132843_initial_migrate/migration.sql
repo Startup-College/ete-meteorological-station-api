bb798a0dcf1fb61b5bcf05834c70cfdabef7057c
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-11-20 14:07:05 -0300

    fix: pop and restart migrate

--- prisma/migrations/20241120132843_initial_migrate/migration.sql
+++ prisma/migrations/20241120132843_initial_migrate/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
 CREATE TABLE "stations" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "latitude" TEXT,
     "longitude" TEXT,
+    "token" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-    "userId" TEXT,
 
     CONSTRAINT "stations_pkey" PRIMARY KEY ("id")
 );
@@ -14,7 +14,7 @@ CREATE TABLE "stations" (
 -- CreateTable
 CREATE TABLE "readings" (
     "id" SERIAL NOT NULL,
-    "station_id" TEXT NOT NULL,
+    "station_id" INTEGER NOT NULL,
     "date_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "temperature" DOUBLE PRECISION NOT NULL,
     "humidity" DOUBLE PRECISION NOT NULL,
@@ -33,9 +33,6 @@ CREATE TABLE "users" (
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
-
--- AddForeignKey
-ALTER TABLE "stations" ADD CONSTRAINT "stations_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "readings" ADD CONSTRAINT "readings_station_id_fkey" FOREIGN KEY ("station_id") REFERENCES "stations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
